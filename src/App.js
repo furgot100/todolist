3f0fec9d9566a49a9fe9a46d0951b906452b0571
Francis Tsang <frtsang40@gmail.com>
2022-03-02 12:55:44 -0800

    now can update states. can edit and filter

--- src/App.js
+++ src/App.js
@@ -4,9 +4,17 @@ import FilterButton from "./components/FilterButton";
 import Todo from "./components/Todo";
 import { nanoid } from "nanoid";
 
+const FILTER_MAP = {
+  All: () => true,
+  Active: task => !task.completed,
+  Completed: task => task.completed
+};
+
+const FILTER_NAMES = Object.keys(FILTER_MAP);
 
 function App(props) {
   const [tasks, setTasks] = useState(props.tasks);
+  const [filter, setFilter] = useState('All');
 
   function addTask(name) {
     const newTask = { id: "todo-" + nanoid(), name: name, completed: false };
@@ -31,28 +39,46 @@ function App(props) {
     setTasks(remainingTasks);
   }
 
-  const taskList = tasks?.map(task => (
-    <Todo
+  function editTask(id, newName) {
+    const editedTaskList = tasks.map(task => {
+    // if this task has the same ID as the edited task
+      if (id === task.id) {
+        //
+        return {...task, name: newName}
+      }
+      return task;
+    });
+    setTasks(editedTaskList);
+  }
+
+  const taskList = tasks?.filter(FILTER_MAP[filter]).map(task => (
+      <Todo
         id={task.id}
         name={task.name}
         completed={task.completed}
         key={task.id}
         toggleTaskCompleted={toggleTaskCompleted}
         deleteTask={deleteTask}
+        editTask={editTask}
       />
-    )
-  );
+  ));
+
+  const filterList = FILTER_NAMES.map(name => (
+    <FilterButton
+      key={name}
+      name={name}
+      isPressed={name === filter}
+      setFilter={setFilter}
+    />
+  ));
 
   return (
     <div className="todoapp stack-large">
       <h1>Todo</h1>
       <Form addTask={addTask} />
       <div className="filters btn-group stack-exception">
-        <FilterButton />
-        <FilterButton />
-        <FilterButton />
+        {filterList}
       </div>
-
 
       {/* ensures the list wont break when we give it certain css */}
       <ul
